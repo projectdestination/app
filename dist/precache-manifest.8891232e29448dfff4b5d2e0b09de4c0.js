f1faf5384d4b116fb3ce782d9079d0eaa6a3c185
Soner Vergon <soner@vergon.se>
2018-08-12 18:38:18 +0200

    A lot of changes

---
+++ dist/precache-manifest.8891232e29448dfff4b5d2e0b09de4c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "daf82bcbaa3af302f5de",
+    "url": "/js/chunk-vendors.daf82bcb.js"
+  },
+  {
+    "revision": "075d1955fcafb710c9a4",
+    "url": "/js/app.075d1955.js"
+  },
+  {
+    "revision": "0880422ddd1d899b24fb",
+    "url": "/js/about.0880422d.js"
+  },
+  {
+    "revision": "df44f6a234e377c3162215d12e9e8e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075d1955fcafb710c9a4",
+    "url": "/css/app.e640ec14.css"
+  }
+];
