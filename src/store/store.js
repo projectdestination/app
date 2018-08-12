bbdb219d4d87733a40d89dc37e7345515735979b
Soner Vergon <soner@vergon.se>
2018-08-12 22:12:59 +0200

    A loot of changes

--- src/store/store.js
+++ src/store/store.js
@@ -7,7 +7,7 @@ import "firebase/firestore";
 import "firebase/auth";
 import firebaseConfig from "../config.js";
 
-// Initialize
+// Initialized
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 const auth = firebase.auth();
