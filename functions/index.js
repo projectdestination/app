9e02e58e9e6e055d008e4c707f2806d504baeba9
Soner Vergon <soner@vergon.se>
2018-10-06 19:41:51 +0200

    Firebase function for new company added

--- functions/index.js
+++ functions/index.js
@@ -16,3 +16,13 @@ exports.slackNewUser = functions.auth.user().onCreate(user => {
     { json: { text: `New user: ${email}.` } }
   );
 });
+
+exports.slackNewCompany = functions.firestore
+  .document("companies/{company_key}")
+  .onCreate(snapshot => {
+    const company = snapshot.data().display_name;
+    return request.post(
+      "https://hooks.slack.com/services/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",
+      { json: { text: `New company added: ${company}.` } }
+    );
+  });
