6ca712bc8adfcbf869ec36ade4eb0ad089645620
Soner Vergon <soner@vergon.se>
2018-10-06 19:07:04 +0200

    Added tracking to company table

--- src/components/app/company/CompanyTable.vue
+++ src/components/app/company/CompanyTable.vue
@@ -41,6 +41,11 @@ export default {
   },
   methods: {
     openModal(data) {
+      this.$ga.event({
+        eventCategory: "Company",
+        eventAction: "Clicked company in table",
+        eventValue: data.company_key
+      });
       this.$modal.open({
         parent: this,
         component: EditCompany,
