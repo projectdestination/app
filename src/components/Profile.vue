deb235380ed1021a2fdb27e75d4953b0eb98d627
Soner Vergon <soner@vergon.se>
2018-10-17 12:32:01 +0200

    Save

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,14 +3,14 @@
   <div class="media-content">
     <figure class="media-left">
       <p class="image is-90x90">
-        <img class="img" src="https://bulma.io/images/placeholders/128x128.png">
+        <img class="img" :src="data.image">
       </p>
     </figure>
     <div class="content pd-font">
       <p class="pd-font">
-        <strong>Soner Vergon</strong> <br /> <small><span class="uppercase spacing">Art Director</span></small>
+        <strong><span class="is-5 title">{{data.name}}</span></strong> <br /> <small><span class="uppercase spacing subtitle is-6">{{data.role}}</span></small>
         <br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+        {{data.text}}
       </p>
     </div>
   </div>
@@ -19,7 +19,10 @@
 
 <script>
 export default {
-  name: "Profile"
+  name: "Profile",
+  props: {
+    data: Object
+  }
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,47 +6,123 @@
       <HeroSmall v-if="data.type === 'heroSmall'" :isHome="true" v-bind:login="login" v-bind:data="data"/>
       <!--<Level v-if="data.type === 'level'" v-bind:data="data" />-->
     </span>
-    <section class="section pd-intro">
-      <div class="container">
-        <div class="content pd-font">
-          <h1 class="pd-font uppercase spacing has-text-centered">What is project destination?</h1>
-          <div class="columns content-container">
-            <div class="column is-one-quarter"></div>
-            <div class="column is-one-quarter has-background-pd-light">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+      <section class="hero pd-intro is-medium  is-pd-pink is-bold">
+        <div style="position:absolute;">
+          asdasdasd
+        </div>
+        <div class="hero-body">
+          <div class="container has-text-left">
+            <div class="tile is-ancestor">
+              <div class="tile is-parent">
+                <div class="tile is-5 is-child">
+                  <h1 class="title pd-font uppercase spacing">
+                    project destination?
+                  </h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+                <div  class="tile is-child">
+                  <div class="container">
+                    <img class="is-pulled-ri" width="200px" src="@/assets/logo.png" />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="column is-one-quarter has-background-pd-pink"></div>
-            <div class="column is-one-quarter has-background-pd-blue"></div>
+            <div class="tile is-ancestor margin">
+              <div class="tile is-parent">
+                <div class="tile is-child">
+                </div>
+                <div class="tile has-text-right is-5 is-child">
+                  <h1 class="title pd-font uppercase spacing">
+                    For students by students
+                  </h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+
+              </div>
+            </div>
+            <div class="tile is-ancestor margin">
+              <div class="tile is-parent">
+
+                <div class="tile has-text-left is-5 is-child">
+                  <h1 class="title pd-font uppercase spacing">
+                    Trancparancy is key
+                  </h1>
+                  <h2 class="subtitle pd-font uppercase spacing">
+                    The project destination app
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+                <div class="tile is-child">
+                  <div class="container">
+                    <a v-on:click="login"  class="button is-home">{{user ? "Enter app" : "Log in"}}</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <div class="hero pd-intro is-white">
+        <div class="hero-body has-text-centered">
+          <h1 class="title pd-font uppercase spacing">
+            The team
+          </h1>
+          <h2 class="subtitle pd-font uppercase spacing">
+            18/19
+          </h2>
+          <div class="container">
+            <div class="content pd-font">
+              <div class="columns content-container">
+                <div class="column">
+                  <Profile :data="team.soner" />
+                </div>
+                <div class="column">
+                  <Profile :data="team.erik" />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+              </div>
+              <div class="columns content-container">
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+              </div>
+              <div class="columns content-container">
+                <div class="column is-one-quarter">
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                  <Profile />
+                </div>
+                <div class="column">
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <section class="section pd-team">
-      <div class="container">
-        <div class="content pd-font">
-          <div class="columns content-container ">
-            <div class="column is-one-quarter has-background-pd-green"><h1 class="pd-font uppercase spacing has-text-centered">The team</h1></div>
-            <div class="column is-one-quarter">
-              <Profile />
-              <Profile />
-              <Profile />
-            </div>
-            <div class="column is-one-quarter">
-              <Profile />
-              <Profile />
-              <Profile />
-            </div>
-            <div class="column is-one-quarter">
-              <Profile />
-              <Profile />
-              <Profile />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
   </div>
 </template>
 <script>
@@ -72,7 +148,24 @@ export default {
   },
   data() {
     return {
-      items: []
+      team: {
+        soner: {
+          name: "Soner Vergon",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut.",
+          role: "Art Director",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/project-destination.appspot.com/o/images%2Fteam%2Fprofil.jpg?alt=media&token=6d710bf2-2b8d-474e-8390-d7d57aa3c7fb"
+        },
+        erik: {
+          name: "Erik Tällberg",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. ",
+          role: "Project Manager",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/project-destination.appspot.com/o/images%2Fteam%2Fweberik.png?alt=media&token=8eb06068-be22-42ac-876a-b36c34a2686a"
+        }
+      }
     };
   },
   methods: {
@@ -91,15 +184,12 @@ export default {
 };
 </script>
 <style scoped>
-.content {
-  height: 100% !important;
+.margin {
+  margin-top: 6% !important;
 }
-.container {
-  height: 100% !important;
-}
+
 .pd-intro {
-  padding-top: 7%;
-  height: 90vh;
+  margin-top: 0%;
 }
 .content-container {
   margin: 7% 0;
